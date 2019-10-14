681757ded2fc04c42e41ea3bec98e80ef36bdd7d
Sven van Straalen <ssgvanstraalen@gmail.com>
2019-10-14 20:02:19 +0200

    change webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,35 +1,4 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const path = require('path');
+const feConfig = require('./webpack.config.fe');
+const serverConfig = require('./webpack.config.server');
 
-module.exports = {
-  mode: 'development',
-  entry: './src/fe/main.js',
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'main.js'
-  },
-  resolve: {
-    alias: {
-      Characters: path.resolve(__dirname, 'src/fe/characters/'),
-      Fx: path.resolve(__dirname, 'src/fe/fx/'),
-      Assets: path.resolve(__dirname, 'src/fe/assets/'),
-      Utils: path.resolve(__dirname, 'src/fe/utils/')
-    }
-  },
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: "babel-loader"
-        }
-      },
-      {
-        test: /\.(gif|png|jpe?g|svg|xml|ogg|wav)$/i,
-        use: "file-loader"
-      }
-    ]
-  },
-  plugins: [new HtmlWebpackPlugin()]
-};
+module.exports = [feConfig, serverConfig];
